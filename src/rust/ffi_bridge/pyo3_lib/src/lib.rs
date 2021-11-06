9275464cc6f728b8b950a33af57f7c17a78d7270
Eric Arellano <ericarellano@me.com>
2021-11-06 16:54:19 -0700

    Separate PyO3 lib from native extension crate

--- src/rust/ffi_bridge/pyo3_lib/src/lib.rs
+++ src/rust/ffi_bridge/pyo3_lib/src/lib.rs
@@ -1,1 +1,22 @@
+use pyo3::prelude::*;
 
+#[derive(Clone, Debug)]
+pub struct Person {
+    pub name: String,
+    pub age: u32,
+}
+
+#[pyclass]
+pub struct PyPerson(pub Person);
+
+#[pymethods]
+impl PyPerson {
+    #[new]
+    fn __new__(name: String, age: u32) -> PyPerson {
+        PyPerson(Person { name, age })
+    }
+
+    fn __repr__(&self) -> String {
+        format!("PyPerson(name='{}', age={})", self.0.name, self.0.age)
+    }
+}
