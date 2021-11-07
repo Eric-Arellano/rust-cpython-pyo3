66acf233a7a907ed9041793604f8ac1df3295748
Eric Arellano <ericarellano@me.com>
2021-11-06 17:12:54 -0700

    Set up Rust-CPython extension!

--- src/python/ffi_bridge/main.py
+++ src/python/ffi_bridge/main.py
@@ -1,9 +1,10 @@
-from ffi_bridge import pyo3_extension
+from ffi_bridge import cpython_extension, pyo3_extension
 
 
 def main() -> None:
     pyo3_person = pyo3_extension.PyPerson("some user", 23)
     print(pyo3_person)
+    print(cpython_extension.greet_py_person())
 
 
 if __name__ == '__main__':
--- src/rust/ffi_bridge/src/lib.rs
+++ src/rust/ffi_bridge/src/lib.rs
@@ -1,1 +1,20 @@
+use cpython::{py_fn, py_module_initializer, PyResult, Python};
+use pyo3_lib::{Person, PyPerson};
 
+py_module_initializer!(cpython_extension, |py, m| {
+    #[allow(clippy::manual_strip)]
+    m.add(py, "greet_py_person", py_fn!(py, greet_py_person()))?;
+    Ok(())
+});
+
+fn greet_py_person(_: Python) -> PyResult<String> {
+    // TODO: read a PyPerson from FFI pointer.
+    let py_person = PyPerson(Person {
+        name: "hardcoded".to_owned(),
+        age: 23,
+    });
+    Ok(format!(
+        "Hello, {}! You are {} years old, cool!",
+        py_person.0.name, py_person.0.age
+    ))
+}
